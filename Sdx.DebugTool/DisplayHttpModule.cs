e9c09d19093a8cfcf822a635467c6356b11548ca
Masamoto Miyata <miyata@sincere-co.com>
2015-06-23 13:50:59 +0900

    Filterをクラシックモードで使うと規定のドッキュメントが効かなくなるので、単純にお知りにつけるようにしました。

--- Sdx.DebugTool/DisplayHttpModule.cs
+++ Sdx.DebugTool/DisplayHttpModule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Sdx.DebugTool
 {
@@ -8,15 +9,67 @@ namespace Sdx.DebugTool
   {
     public void Init(HttpApplication application)
     {
-      application.BeginRequest += new EventHandler(app_BeginRequest);
+      application.BeginRequest += new EventHandler(Application_BeginRequest);
+      application.EndRequest += new EventHandler(this.Application_EndRequest);
     }
 
     public void Dispose() { }
 
-    void app_BeginRequest(object sender, EventArgs a)
+    private void Application_BeginRequest(object source, EventArgs a)
     {
       Debug.initRequestTimer();
-      HttpContext.Current.Response.Filter = new Sdx.DebugTool.DisplayResponseFilter(HttpContext.Current.Response.Filter);
+    }
+
+    private void Application_EndRequest(object source, EventArgs a)
+    {
+      HttpApplication application = (HttpApplication)source;
+      HttpContext context = application.Context;
+
+      writeDebugLogs(context);
+    }
+
+    private void writeDebugLogs(HttpContext context)
+    {
+      if(Debug.Logs.Count == 0)
+      {
+        return;
+      }
+
+      var debugString = "<div class=\"sdx-debug-wrapper\">";
+      Int64 prevElapsed = -1;
+      foreach (Dictionary<String, Object> dic in Debug.Logs)
+      {
+        var totalElapsed = (Int64)dic["elapsedMsec"];
+        Int64 currentElapsed = 0;
+        if (prevElapsed != -1)
+        {
+          currentElapsed = totalElapsed - prevElapsed;
+        }
+
+        debugString += WrapTag(
+          Debug.Dump(dic["value"]),
+          dic["title"] as String,
+          totalElapsed,
+          currentElapsed
+        );
+
+        prevElapsed = totalElapsed;
+      }
+      debugString += "</div>";
+
+      context.Response.Write(debugString);
+    }
+
+    private String WrapTag(String value, String title, Int64 totalElapsed, Int64 currentElapsed)
+    {
+      DateTime now = DateTime.Now;
+      return
+        "<div class=\"sdx-debug-item\">" +
+          "<div class=\"sdx-debug-title\">" +
+            "[" + currentElapsed / 1000.0 + "/" + totalElapsed / 1000.0 + "]" + title +
+          "</div>" +
+          "<pre class=\"sdx-debug-value\">" + value + "</pre>" +
+        "</div>";
     }
   }
 }
