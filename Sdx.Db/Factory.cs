7e639b170dfad50a98750f2c27f3b78de8422420
Masamoto Miyata <miyata@sincere-co.com>
2015-06-30 16:51:27 +0900

    DbProviderFactoryという便利なクラスがあったのでそっちを使うように変更。Adapterは廃止。

---
+++ Sdx.Db/Factory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Sdx.Db
+{
+  public abstract class Factory
+  {
+    DbProviderFactory factory;
+
+    protected abstract DbProviderFactory GetFactory();
+
+    public string ConnectionString { get; set; }
+
+    public Factory()
+    {
+      this.factory = this.GetFactory();
+    }
+
+    public DbConnection CreateConnection()
+    {
+      DbConnection con = this.factory.CreateConnection();
+      con.ConnectionString = this.ConnectionString;
+      return con;
+    }
+
+    public DbParameter CreateParameter(string key, string value)
+    {
+      DbParameter param = this.factory.CreateParameter();
+      param.ParameterName = key;
+      param.Value = value;
+
+      return param;
+    }
+
+    public Where CreateWhere()
+    {
+      return new Where(this);
+    }
+
+    public DbCommand CreateDbCommand()
+    {
+      return this.factory.CreateCommand();
+    }
+
+    public DbCommandBuilder CreateCommandBuilder()
+    {
+      return this.factory.CreateCommandBuilder();
+    }
+  }
+}
---
+++ Sdx.Db/SqlServerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Sdx.Db
+{
+  public class SqlServerFactory : Factory
+  {
+    override protected DbProviderFactory GetFactory()
+    {
+      return DbProviderFactories.GetFactory("System.Data.SqlClient");
+    }
+  }
+}
