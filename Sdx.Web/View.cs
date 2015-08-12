495fd73c768ca32a38117f58c12870a4bddeff4a
Masamoto Miyata <miyata@sincere-co.com>
2015-08-12 16:42:19 +0900

    Controlの継承を使わなくて済むように変更しました。

---
+++ Sdx.Web/View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Sdx.Web
+{
+  public class View
+  {
+    private static String varsKey = "SDX.WEB.VIEW.VARS";
+    public static Holder Vars
+    {
+      get
+      {
+        if(!HttpContext.Current.Items.Contains(varsKey))
+        {
+          HttpContext.Current.Items[varsKey] = new Holder();
+        }
+
+        return (Holder)HttpContext.Current.Items[varsKey];
+      }
+    }
+  }
+}
