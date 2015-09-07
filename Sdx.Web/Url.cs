2ade830a292d8dc56a523165cfeace527a20cc40
tomita-ychr <sunrisedigitaltomita@gmail.com>
2015-09-07 13:52:25 +0900

    コンストラクタの内部動作を追加
    直接 System.Uri クラス使ってもよかったかもしれませんが
    クエリ文字列からキーごとに値を取得するのは System.Uri クラスの
    メソッドではできなさそう？だったので、とりあえずSdx.Web.Uriのコンストラクターで
    インスタンスの生成をして、パラメータは自前で連想配列を作成してキーごとに値を格納して
    みました。

--- Sdx.Web/Url.cs
+++ Sdx.Web/Url.cs
@@ -7,9 +7,24 @@ namespace Sdx.Web
 {
   public class Url
   {
-    public Url(String str)
-    {
+    private Uri _uri;
+    private Dictionary<string, string> _query_data;
 
+    //コンストラクタ
+    public Url(string url_str)
+    {
+      //@System.Uri
+      this._uri = new Uri(url_str);
+
+      //クエリを分解して連想配列にする
+      string[] param_list = this._uri.Query.Split('&');
+      Dictionary<string, string> data = new Dictionary<string, string>();
+      foreach (var item in param_list)
+      {
+        string[] item_part = item.Split('=');
+        data.Add(item_part[0], item_part[1]);
+      }
+      this._query_data = data;
     }
   }
 }
