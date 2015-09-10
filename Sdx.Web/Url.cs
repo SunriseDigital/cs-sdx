e50dc4d8baa8b4f6bb3b981a8cc84d098e3c9f51
tomita-ychr <sunrisedigitaltomita@gmail.com>
2015-09-10 14:30:38 +0900

    BuildQueryString() 内で引数の型で処理を分岐していたのをやめる
    以前の動作に戻しています。
    BuildQueryString() は、あくまで「引数を元にURL文字列を生成する」動作だけを
    担当することになるので、メソッドの責任としてもこのほうがよさそうです。

--- Sdx.Web/Url.cs
+++ Sdx.Web/Url.cs
@@ -52,43 +52,15 @@ namespace Sdx.Web
       this.paramData[key] = value;
     }
 
-    private string BuildQueryString(object param = null)
+    private string BuildQueryString(Dictionary<string,string> param)
     {
       //ここに格納された値を最終的に "&" で連結してクエリ文字列にします
       var baseParams = new List<string>();
-      foreach (KeyValuePair<string, string> item in this.paramData)
+      foreach (KeyValuePair<string, string> pair in param)
       {
-        var tmp = string.Format("{0}={1}", item.Key, item.Value);
+        var tmp = string.Format("{0}={1}", pair.Key, pair.Value);
         baseParams.Add(tmp);
       }
-
-      if(param is Dictionary<string, string>)
-      {
-        //コンパイル時には型が確定していないためキャストしています
-        var dicParams = (Dictionary<string, string>)param;
-        foreach (KeyValuePair<string, string> pair in dicParams)
-        {
-          var tmp = string.Format("{0}={1}", pair.Key, pair.Value);
-          baseParams.Add(tmp);
-        }
-      }
-
-      if(param is List<string>)
-      {
-        //コンパイル時には型が確定していないためキャストしています
-        var excludeList = (List<string>)param;
-        var excludedData = this.paramData
-          .Where(p => excludeList.Contains(p.Key) == false)
-          .ToList()//この時点で List<KeyValuePair<string,string>> 型のListになる
-        ;
-        var tmplist = new List<string>();
-        foreach (KeyValuePair<string, string> data in excludedData)
-        {
-          tmplist.Add(string.Format("{0}={1}", data.Key, data.Value));
-        }
-        baseParams = tmplist;
-      }
-
       var query = "";
       if(baseParams.Count > 0)
       {
@@ -108,21 +80,37 @@ namespace Sdx.Web
     public string Build()
     {
       string path = this.BuildPath();
-      string query = this.BuildQueryString();
+      string query = this.BuildQueryString(this.paramData);
       return path + query;
     }
 
     public string Build(Dictionary<string, string> param)
     {
       string path = this.BuildPath();
-      string query = this.BuildQueryString(param);
+      var list = new Dictionary<string, string>();
+
+      foreach (KeyValuePair<string, string> item in this.paramData)
+      {
+        list[item.Key] = item.Value;
+      }
+
+      foreach (KeyValuePair<string, string> item in param)
+      {
+        list[item.Key] = item.Value;
+      }
+
+      string query = this.BuildQueryString(list);
       return path + query;
     }
 
-    public string Build(List<string> param)
+    public string Build(List<string> excludeList)
     {
       string path = this.BuildPath();
-      string query = this.BuildQueryString(param);
+      var excludedDic = this.paramData
+        .Where(p => excludeList.Contains(p.Key) == false)
+        .ToDictionary(p => p.Key, p => p.Value)
+      ;
+      string query = this.BuildQueryString(excludedDic);
       return path + query;
     }
   }
