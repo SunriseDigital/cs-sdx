816875f140a3cacfed6919b75fc5331ace60428c
Masamoto Miyata <miyata@sincere-co.com>
2015-10-07 16:12:54 +0900

    いくつかのユーティリティークラスをついか。

---
+++ Sdx/Util/Random.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace Sdx.Util
+{
+  public static class Number
+  {
+    private static ThreadLocal<Random> randomWrapper = new ThreadLocal<System.Random>(() =>
+    {
+      using (var rng = new RNGCryptoServiceProvider())
+      {
+        var buffer = new byte[sizeof(int)];
+        rng.GetBytes(buffer);
+        var seed = BitConverter.ToInt32(buffer, 0);
+        return new Random(seed);
+      }
+    });
+
+    /// <summary>
+    /// スレッドセイフでEnviroment.TickCountに左右されないRandomを生成します。
+    /// </summary>
+    /// <returns></returns>
+    public static Random CreateRandom()
+    {
+      return randomWrapper.Value;
+    }
+  }
+}
---
+++ Sdx/Util/String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdx.Util
+{
+  public static class String
+  {
+    private const string RandomSeeds = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static string GenRandom(int length)
+    {
+      var rnd = Number.CreateRandom();
+      var builder = new StringBuilder();
+      for(var i = 0; i < length; i++)
+      {
+        var index = rnd.Next(0, RandomSeeds.Length - 1);
+        builder.Append(RandomSeeds[index]);
+      }
+
+      return builder.ToString();
+    }
+  }
+}
