7da67491bbd4539e9b041a2b3436f7fac384c832
Masamoto Miyata <miyata@sincere-co.com>
2016-02-15 17:23:11 +0900

    Groupingのテストを追加。

--- UnitTest/ScaffoldTest.cs
+++ UnitTest/ScaffoldTest.cs
@@ -10,6 +10,10 @@ using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
 #endif
 
 using System;
+using Moq;
+using System.Collections.Specialized;
+using System.Web;
+using System.IO;
 
 namespace UnitTest
 {
@@ -112,6 +116,71 @@ namespace UnitTest
       Assert.IsType<Sdx.Html.InputText>(form["code"]);
       Assert.Equal("コード", form["code"].Label);
       Assert.Equal("code", form["code"].Tag.Attr["name"]);
+    }
+
+    [Fact]
+    public void TestGrouping()
+    {
+      foreach (TestDb db in this.CreateTestDbList())
+      {
+        RunGrouping(db);
+        ExecSql(db);
+      }
+    }
+
+    private void RunGrouping(TestDb db)
+    {
+      HttpContext.Current = new HttpContext(
+        new HttpRequest("", "http://wwww.example.com", "large_area_id=1"),
+        new HttpResponse(new StringWriter())
+      );
+
+      ((Action)(() => 
+      {
+        var scaffold = new Sdx.Scaffold.Manager(Test.Orm.Table.Area.Meta, db.Adapter, db.Adapter.ToString());
+        scaffold.EditPageUrl = new Sdx.Web.Url("/scaffold/area/edit.aspx");
+        scaffold.ListPageUrl = new Sdx.Web.Url("/scaffold/area/list.aspx");
+
+        scaffold.Group = new Sdx.Scaffold.Group.TableMeta("large_area_id", Test.Orm.Table.LargeArea.Meta, "name");
+
+        scaffold.InitGroup();
+
+        Assert.Equal("/scaffold/area/edit.aspx?large_area_id=1", scaffold.EditPageUrl.Build());
+        Assert.Equal("/scaffold/area/list.aspx?large_area_id=1", scaffold.ListPageUrl.Build());
+
+        var actualSet = scaffold.FetchRecordSet();
+        actualSet.ForEach((rec) =>
+        {
+          Assert.Equal("1", rec.GetString("large_area_id"));
+        });      
+      }))();
+
+
+      HttpContext.Current = new HttpContext(
+        new HttpRequest("", "http://wwww.example.com", "large_area_id=2"),
+        new HttpResponse(new StringWriter())
+      );
+
+      ((Action)(() =>
+      {
+        var scaffold = new Sdx.Scaffold.Manager(Test.Orm.Table.Area.Meta, db.Adapter, db.Adapter.ToString());
+        scaffold.EditPageUrl = new Sdx.Web.Url("/scaffold/area/edit.aspx");
+        scaffold.ListPageUrl = new Sdx.Web.Url("/scaffold/area/list.aspx");
+
+        scaffold.Group = new Sdx.Scaffold.Group.TableMeta("large_area_id", Test.Orm.Table.LargeArea.Meta, "name");
+
+        scaffold.InitGroup();
+
+        Assert.Equal("/scaffold/area/edit.aspx?large_area_id=2", scaffold.EditPageUrl.Build());
+        Assert.Equal("/scaffold/area/list.aspx?large_area_id=2", scaffold.ListPageUrl.Build());
+
+        var actualSet = scaffold.FetchRecordSet();
+        actualSet.ForEach((rec) =>
+        {
+          Assert.Equal("2", rec.GetString("large_area_id"));
+        });
+      }))();
+
     }
   }
 }
