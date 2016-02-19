3288f960aab8336159325048bd7cedbcc014d947
Masamoto Miyata <miyata@sincere-co.com>
2016-02-19 12:43:23 +0900

    Managerを実装しました。

---
+++ Sdx/Db/Adapter/Manager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdx.Db.Adapter
+{
+  public class Manager
+  {
+    private List<Base> writeAdapters = new List<Base>();
+    private List<Base> readAdapters = new List<Base>();
+
+    private static Random random = new Random();
+
+    public void AddCommonAdapter(Base adapter)
+    {
+      writeAdapters.Add(adapter);
+      readAdapters.Add(adapter);
+    }
+
+    public void AddReadAdapter(Base adapter)
+    {
+      readAdapters.Add(adapter);
+    }
+
+    public void AddWriteAdapter(Base adapter)
+    {
+      writeAdapters.Add(adapter);
+    }
+
+    public Base Read
+    {
+      get
+      {
+        if(readAdapters.Count == 0)
+        {
+          throw new InvalidOperationException("Read Adapter is empty.");
+        }
+        else if(readAdapters.Count == 1)
+        {
+          return readAdapters[0];
+        }
+        else
+        {
+          //Random.Next(maxValue)はmaxValueを含みません
+          return readAdapters[random.Next(readAdapters.Count)];
+        }
+      }
+    }
+
+    public Base Write
+    {
+      get
+      {
+        if (writeAdapters.Count == 0)
+        {
+          throw new InvalidOperationException("Read Adapter is empty.");
+        }
+        else if (writeAdapters.Count == 1)
+        {
+          return writeAdapters[0];
+        }
+        else
+        {
+          return writeAdapters[random.Next(writeAdapters.Count)];
+        }
+      }
+    }
+
+    public Base Share
+    {
+      get
+      {
+        var totalCount = writeAdapters.Count + readAdapters.Count;
+        var seed = random.Next(totalCount);
+
+        if(seed < writeAdapters.Count)
+        {
+          return writeAdapters[seed];
+        }
+        else
+        {
+          return readAdapters[seed - writeAdapters.Count];
+        }
+      }
+    }
+  }
+}
---
+++ UnitTest/DbAdapterTest.cs
@@ -0,0 +1,251 @@
+﻿using Xunit;
+using UnitTest.DummyClasses;
+
+#if ON_VISUAL_STUDIO
+using FactAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using ClassInitializeAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using ClassCleanupAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTest
+{
+  [TestClass]
+  public class DbAdapterTest : BaseTest
+  {
+    [ClassInitialize]
+    public static void InitilizeClass(TestContext context)
+    {
+      Console.WriteLine("FixtureSetUp");
+      //最初のテストメソッドを実行する前に一回だけ実行したい処理はここ
+    }
+
+    [ClassCleanup]
+    public static void CleanupClass()
+    {
+      Console.WriteLine("FixtureTearDown");
+      //全てのテストメソッドが実行された後一回だけ実行する処理はここ
+    }
+
+
+
+    override public void FixtureSetUp()
+    {
+      DbAdapterTest.InitilizeClass(null);
+      //ここのクラス名は適宜書き換えてください。
+      //MSTestのFixtureSetUpがstaticじゃないとだめだったのでこのような構造になってます。
+    }
+
+    override public void FixtureTearDown()
+    {
+      DbAdapterTest.CleanupClass();
+      //@see FixtureSetUp
+    }
+
+
+    [Fact]
+    public void TestManagerOnlyRead()
+    {
+      var main = new Sdx.Db.Adapter.Manager();
+
+      for (int i = 0; i < 5; i++)
+      {
+        var db = new Sdx.Db.Adapter.SqlServer();
+        db.ConnectionString = "SqlServer read " + i.ToString();
+        main.AddReadAdapter(db);
+      }
+
+      var connStr = new List<string>();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Read;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer read"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      connStr.Clear();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Share;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer read"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      Exception ex2 = Record.Exception(new Assert.ThrowsDelegate(() =>
+      {
+        var db = main.Write;
+      }));
+
+      Assert.IsType<InvalidOperationException>(ex2);
+    }
+
+    [Fact]
+    public void TestManagerOnlyWrite()
+    {
+      var main = new Sdx.Db.Adapter.Manager();
+
+      for (int i = 0; i < 5; i++)
+      {
+        var db = new Sdx.Db.Adapter.SqlServer();
+        db.ConnectionString = "SqlServer write " + i.ToString();
+        main.AddWriteAdapter(db);
+      }
+
+      var connStr = new List<string>();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Write;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer write"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      connStr.Clear();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Share;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer write"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      Exception ex2 = Record.Exception(new Assert.ThrowsDelegate(() =>
+      {
+        var db = main.Read;
+      }));
+
+      Assert.IsType<InvalidOperationException>(ex2);
+    }
+
+    [Fact]
+    public void TestManagerBothOneWrite()
+    {
+      var main = new Sdx.Db.Adapter.Manager();
+
+      for (int i = 0; i < 5; i++)
+      {
+        var db = new Sdx.Db.Adapter.SqlServer();
+        db.ConnectionString = "SqlServer read " + i.ToString();
+        main.AddReadAdapter(db);
+      }
+
+      for (int i = 0; i < 1; i++)
+      {
+        var db = new Sdx.Db.Adapter.SqlServer();
+        db.ConnectionString = "SqlServer write " + i.ToString();
+        main.AddWriteAdapter(db);
+      }
+      
+
+      var connStr = new List<string>();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Read;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer read"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      connStr.Clear();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Write;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer write"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(1, connStr.Count);
+
+      connStr.Clear();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Share;
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(6, connStr.Count);
+    }
+
+    [Fact]
+    public void TestManagerBothMutipleWrite()
+    {
+      var main = new Sdx.Db.Adapter.Manager();
+
+      for (int i = 0; i < 5; i++)
+      {
+        var db = new Sdx.Db.Adapter.SqlServer();
+        db.ConnectionString = "SqlServer read " + i.ToString();
+        main.AddReadAdapter(db);
+      }
+
+      for (int i = 0; i < 5; i++)
+      {
+        var db = new Sdx.Db.Adapter.SqlServer();
+        db.ConnectionString = "SqlServer write " + i.ToString();
+        main.AddWriteAdapter(db);
+      }
+
+
+      var connStr = new List<string>();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Read;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer read"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      connStr.Clear();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Write;
+        Assert.True(db.ConnectionString.StartsWith("SqlServer write"));
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(5, connStr.Count);
+
+      connStr.Clear();
+      for (int i = 0; i < 300; i++)
+      {
+        var db = main.Share;
+        if (!connStr.Any(str => str == db.ConnectionString))
+        {
+          connStr.Add(db.ConnectionString);
+        }
+      }
+      Assert.Equal(10, connStr.Count);
+    }
+  }
+}
