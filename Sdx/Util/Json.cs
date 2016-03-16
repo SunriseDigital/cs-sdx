860ba4ccebe45e46233c9d38a79589999da438e9
Masamoto Miyata <miyata@sincere-co.com>
2016-03-16 16:46:04 +0900

    Util.Jsonを作成。

---
+++ Sdx/Util/Json.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace Sdx.Util
+{
+  public static class Json
+  {
+    public static string Encoder(object obj)
+    {
+      return new JavaScriptSerializer().Serialize(obj);
+    }
+
+    public static dynamic Decode(string json)
+    {
+      return new JavaScriptSerializer().Deserialize<dynamic>(json);
+    }
+  }
+}
