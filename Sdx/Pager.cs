8660f21a6368fe7daa489c720fd31bb4222eaf7e
Masamoto Miyata <miyata@sincere-co.com>
2016-03-24 16:57:01 +0900

    Pagerを追加。とりあえず最低限。

---
+++ Sdx/Pager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdx
+{
+  public class Pager
+  {
+    private int? lastPage;
+    private int? perPage;
+    private int? totalCount;
+
+    private void SetPage(string page)
+    {
+      Page = page == null ? 1 : Int32.Parse(page);
+    }
+
+    public void SetPageData(string page, bool hasNext)
+    {
+      SetPage(page);
+      HasNext = hasNext;
+    }
+
+    public void SetPageData(string page, int perPage, int totalCount)
+    {
+      SetPage(page);
+
+      this.perPage = perPage;
+      this.totalCount = totalCount;
+
+      this.lastPage = TotalCount / PerPage;
+      if (this.totalCount % this.perPage != 0)
+      {
+        ++this.lastPage;
+      }
+
+      HasNext = Page < this.lastPage;
+    }
+
+    public int LastPage
+    {
+      get
+      {
+        if (lastPage == null)
+        {
+          throw new InvalidOperationException("You must call `SetPageData(string int int)` before use this.");
+        }
+        return (int)lastPage;
+      }
+    }
+
+    public int PerPage
+    {
+      get
+      {
+        if (perPage == null)
+        {
+          throw new InvalidOperationException("You must call `SetPageData(string int int)` before use this.");
+        }
+        return (int)perPage;
+      }
+    }
+
+    public int TotalCount
+    {
+      get
+      {
+        if (totalCount == null)
+        {
+          throw new InvalidOperationException("You must call `SetPageData(string int int)` before use this.");
+        }
+        return (int)totalCount;
+      }
+    }
+
+    public int Page { get; private set; }
+
+    public bool HasNext { get; private set; }
+
+    public bool HasPrev
+    {
+      get
+      {
+        if (Page == 1)
+        {
+          return false;
+        }
+
+        return true;
+      }
+    }
+  }
+}
---
+++ UnitTest/Sdx/Pager.cs
@@ -0,0 +1,89 @@
+﻿using Xunit;
+using UnitTest.DummyClasses;
+using Moq;
+using System.Collections.Specialized;
+using System.Web;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Reflection;
+
+#if ON_VISUAL_STUDIO
+using FactAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using ClassInitializeAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using ClassCleanupAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+#endif
+
+using System;
+
+namespace UnitTest
+{
+  [TestClass]
+  public class Pager : BaseTest
+  {
+    [Fact]
+    public void TestTotalCount()
+    {
+      var pager = new Sdx.Pager();
+      pager.SetPageData("1", 10, 38);
+
+      Assert.Equal(4, pager.LastPage);
+      Assert.Equal(10, pager.PerPage);
+      Assert.Equal(38, pager.TotalCount);
+      Assert.Equal(1, pager.Page);
+      Assert.True(pager.HasNext);
+      Assert.False(pager.HasPrev);
+
+      pager.SetPageData("2", 10, 38);
+      Assert.Equal(2, pager.Page);
+      Assert.True(pager.HasNext);
+      Assert.True(pager.HasPrev);
+
+      pager.SetPageData("4", 10, 38);
+      Assert.Equal(4, pager.Page);
+      Assert.False(pager.HasNext);
+      Assert.True(pager.HasPrev);
+    }
+
+    [Fact]
+    public void TestSimple()
+    {
+      var pager = new Sdx.Pager();
+      pager.SetPageData("1", true);
+
+      Assert.Equal(1, pager.Page);
+      Assert.True(pager.HasNext);
+      Assert.False(pager.HasPrev);
+
+      Exception ex;
+      ex = Record.Exception(() => 
+      {
+        Assert.Equal(4, pager.LastPage);
+      });
+
+      Assert.IsType<InvalidOperationException>(ex);
+
+      ex = Record.Exception(() =>
+      {
+        Assert.Equal(4, pager.PerPage);
+      });
+
+      Assert.IsType<InvalidOperationException>(ex);
+
+      ex = Record.Exception(() =>
+      {
+        Assert.Equal(4, pager.TotalCount);
+      });
+
+      Assert.IsType<InvalidOperationException>(ex);
+
+      pager.SetPageData("5", false);
+
+      Assert.Equal(5, pager.Page);
+      Assert.False(pager.HasNext);
+      Assert.True(pager.HasPrev);
+    }
+  }
+}
