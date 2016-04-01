915f9d4795b892be6cd22f2f837bccede83a143e
Masamoto Miyata <miyata@sincere-co.com>
2016-04-01 18:59:23 +0900

    i18nUnitTestを追加。

---
+++ UnitTest/Sdx/I18n.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using UnitTest.DummyClasses;
+
+
+#if ON_VISUAL_STUDIO
+using FactAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using ClassInitializeAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using ClassCleanupAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+#endif
+
+using System;
+
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+  [TestClass]
+  public class I18n : BaseTest
+  {
+    [Fact]
+    public void Test()
+    {
+
+    }
+  }
+}
