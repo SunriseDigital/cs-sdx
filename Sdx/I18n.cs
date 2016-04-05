251764ed6646331252bf3f8a0ff0d94b67528574
Masamoto Miyata <miyata@sincere-co.com>
2016-04-05 18:34:57 +0900

    キーワード抽出まで

---
+++ Sdx/I18n.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdx
+{
+  public class I18n
+  {
+    public static string GetString(params string[] key)
+    {
+      return key[0];
+    }
+
+    public static string GetPluralString(string key, params int[] counts)
+    {
+      return key;
+    }
+
+    public static string GetQuotedString(params string[] key)
+    {
+      return "\"" + key[0] + "\"";
+    }
+  }
+}
