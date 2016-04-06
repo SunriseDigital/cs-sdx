97003808ab5b147a9618e2382b49558684395f7b
Masamoto Miyata <miyata@sincere-co.com>
2016-04-06 18:02:11 +0900

    実際に言語ファイルからデータを取得するところを実装。

--- Sdx/I18n.cs
+++ Sdx/I18n.cs
@@ -1,25 +1,45 @@
-﻿using System;
+﻿using NGettext;
+using NGettext.Loaders;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Sdx
 {
   public class I18n
   {
-    public static string GetString(params string[] key)
+    private static Dictionary<string, ICatalog> catalogDic = new Dictionary<string, ICatalog>();
+    private static ICatalog GetCatalog(string lang)
     {
-      return key[0];
+      if(!catalogDic.ContainsKey(lang))
+      {
+        var assembly = Assembly.GetExecutingAssembly();
+        var stream = assembly.GetManifestResourceStream("Sdx.locale."+lang+".message.mo");
+
+        if (stream == null)
+        {
+          catalogDic[lang] = new Catalog();
+        }
+        else
+        {
+          catalogDic[lang] = new Catalog(new MoLoader(stream));
+        }
+      }
+
+      return catalogDic[lang];
+    }
+    public static string GetString(string key, params object[] args)
+    {
+      return GetCatalog(Sdx.Context.Current.Lang).GetString(key, args);
     }
 
     public static string GetPluralString(string key, params int[] counts)
     {
       return key;
-    }
-
-    public static string GetQuotedString(params string[] key)
-    {
-      return "\"" + key[0] + "\"";
     }
   }
 }
---
+++ UnitTest/Test/Route/LangRouteHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.Routing;
+
+namespace Test.Route
+{
+  public class LangRouteHandler : IRouteHandler
+  {
+    public LangRouteHandler(string virtualPath)
+    {
+      this.VirtualPath = virtualPath;
+    }
+
+    public string VirtualPath { get; set; }
+
+    public IHttpHandler GetHttpHandler(RequestContext requestContext)
+    {
+      String vPath = VirtualPath
+        .Replace("{controller}", requestContext.RouteData.Values["controller"].ToString())
+        .Replace("{action}", requestContext.RouteData.Values["action"].ToString())
+        ;
+      Sdx.Context.Current.Lang = requestContext.RouteData.Values["lang"].ToString();
+      return BuildManager.CreateInstanceFromVirtualPath(vPath, typeof(System.Web.UI.Page)) as IHttpHandler;
+    }
+  }
+}
