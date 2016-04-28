bfaf8e8f43ada2c9b0c95253942992639a5e3419
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2016-04-28 19:40:04 +0900

    BuildTreeの実装まで。

--- Sdx/Data/TreeJson.cs
+++ Sdx/Data/TreeJson.cs
@@ -5,39 +5,61 @@ using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace Sdx.Data
 {
   public class TreeJson : Tree
   {
-    string BaseJson { get; set; }
+    private string BaseJson { get; set; }
 
     public override void Load(TextReader input)
     {
       var json = JObject.Parse(input.ReadToEnd());
+      this.BaseJson = Sdx.Util.Json.Encoder(json);
     }
 
     public override string ToValue()
     {
-      throw new NotImplementedException();
+      if (this.BaseJson == null)
+      {
+        throw new InvalidCastException("This is root node.");
+      }
+
+      return this.BaseJson;
     }
 
     protected override List<Tree> ToList()
     {
-      throw new NotImplementedException();
+      var list = new List<Tree>();
+      return list;
     }
 
     protected override Tree BuildTree(List<string> paths)
     {
       Sdx.Data.TreeJson Json = new Sdx.Data.TreeJson();
+      var jobject = JObject.Parse(this.BaseJson);
+      
+      foreach (var item in paths)
+      {
+        Json.BaseJson = jobject.GetValue(item).ToString();
+      }
 
-      //Json.BaseJson = Sdx.Util.Json.Encoder();
       return Json;
     }
 
     protected override bool Exsits(List<string> paths)
     {
-      throw new NotImplementedException();
+      foreach(var item in paths){
+        var jobject = JObject.Parse(this.BaseJson);
+        
+        //if ()
+        //{
+        //  return false;
+        //}
+      }
+
+      return true;
     }
   }
 }
