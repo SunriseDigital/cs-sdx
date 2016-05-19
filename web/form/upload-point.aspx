1aa98941d577e27d7848503186bb50bd3e70a973
Masamoto Miyata <miyata@sincere-co.com>
2016-05-19 19:36:01 +0900

    アップロード部分はユーザーコントロールに移動しました。

--- web/form/upload-point.aspx
+++ web/form/upload-point.aspx
@@ -1,1 +1,3 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="upload-point.aspx.cs" Inherits="form_upload_point" %>
+<%@ Register TagPrefix="Upload" TagName="image" Src="~\sdx\_private\cs\uploader\image.ascx" %>
+<Upload:image ID="image" runat="server" MaxWidth="250" MaxHeight="350" />
--- web/form/upload-point.aspx.cs
+++ web/form/upload-point.aspx.cs
@@ -10,28 +10,6 @@ public partial class form_upload_point : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-      Sdx.Context.Current.PreventDebugDisplay = true;
 
-      var formName = Request.Params["name"];
-      var response = new Dictionary<string, List<Dictionary<string, string>>>();
-      response["files"] = new List<Dictionary<string, string>>();
-      for (int i = 0; i < Request.Files.Count; i++)
-			{
-        if (formName != Request.Files.GetKey(i))
-        {
-          continue;
-        }
-        var uploaded = Request.Files.Get(i);
-        var fileData = new Dictionary<string, string>();
-        var path = Page.MapPath("~/tmp/" + uploaded.FileName);
-        uploaded.SaveAs(path);
-        fileData["name"] = Sdx.Util.Path.MapWebPath(path);
-        fileData["size"] = uploaded.ContentLength.ToString();
-        fileData["type"] = uploaded.ContentType;
-        fileData["error"] = null;
-        response["files"].Add(fileData);
-			}
-
-      Sdx.Web.Helper.JsonResponse(response);
     }
 }
