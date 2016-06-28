5718bd3fb6da93915ef5b46c15db090915d10829
tkabeee <tkabee@gmail.com>
2016-06-28 19:06:40 +0900

    AWS S3にファイルアップロードする際に必要な情報のget,setを作成

--- Sdx/Web/Image/S3.cs
+++ Sdx/Web/Image/S3.cs
@@ -7,5 +7,60 @@ namespace Sdx.Web.Image
 {
   public class S3 : Base
   {
+    public S3()
+    {
+    }
+
+    public string AwsAccessKey
+    {
+      get
+      {
+        return this.AwsAccessKey;
+      }
+
+      set
+      {
+        this.AwsAccessKey = value;
+      }
+    }
+
+    public string AwsSecretKey
+    {
+      get
+      {
+        return this.AwsSecretKey;
+      }
+
+      set
+      {
+        this.AwsSecretKey = value;
+      }
+    }
+
+    public string AwsRegion
+    {
+      get
+      {
+        return this.AwsRegion;
+      }
+
+      set
+      {
+        this.AwsRegion = value;
+      }
+    }
+
+    public string BucketName
+    {
+      get
+      {
+        return this.BucketName;
+      }
+
+      set
+      {
+        this.BucketName = value;
+      }
+    }
   }
 }
