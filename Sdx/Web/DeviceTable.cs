cef11d7ba5fb75e7ed3396f5c859cee27c4ea544
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2016-11-22 17:04:39 +0900

    IsMatchとコンストラクタの実装中

--- Sdx/Web/DeviceTable.cs
+++ Sdx/Web/DeviceTable.cs
@@ -15,15 +15,11 @@ namespace Sdx.Web
 {
   public class DeviceTable
   {
-    private string pcUrl { get; set; }
-
-    private string spUrl { get; set; }
-
-    private string mbUrl { get; set; }
-
     private Dictionary<string, object> deviceDic = new Dictionary<string, object>();
 
-    private Dictionary<string, object> urlAndQuery = new Dictionary<string, object>();
+    private Dictionary<string, string> regex = new Dictionary<string, string>();
+
+    Dictionary<string, string> queries = new Dictionary<string, string>();
 
     public enum Device
     {
@@ -32,11 +28,35 @@ namespace Sdx.Web
       Mb
     }
 
-    public DeviceTable(YamlNode pageYaml)
-    {
-      foreach (var item in (YamlMappingNode)pageYaml)
+    public DeviceTable(YamlMappingNode pageYaml)
+    {      
+      foreach (var item in pageYaml)
+      {        
+        if (item.Value is YamlNode)
+        {
+          Dictionary<string, object> deviceUrl = new Dictionary<string, object>();
+          foreach (var value in (YamlMappingNode)item.Value)
+          {
+            
+            if (value.Value is YamlScalarNode)
+            {
+              deviceUrl.Add(value.Key.ToString(), value.Value.ToString());
+            }
+            else
+            {
+              deviceUrl.Add(value.Key.ToString(), value.Value);
+            }            
+          }
+          deviceDic.Add(item.Key.ToString(), deviceUrl);        
+        }
+      }
+
+      if (deviceDic.ContainsKey("query"))
       {
-        deviceDic.Add(item.Key.ToString(), item.Value);
+        foreach (var query in (YamlMappingNode)deviceDic["query"])
+        {
+          queries.Add(query.Key.ToString(), query.Value.ToString());
+        }
       }
     }
 
@@ -87,42 +107,33 @@ namespace Sdx.Web
 
     public bool IsMatch(Device device, string url)
     {
-      //url => "/sp/kanagawa/shop/"
-      //query => "tg_prices_high = 1"
-
-      if (deviceDic["pc"] is YamlNode)
-      {
-        foreach (var value in (YamlMappingNode)deviceDic["pc"])
-        {
-          if (value.Value is YamlScalarNode)
-          {
-            urlAndQuery.Add(value.Key.ToString(), value.Value.ToString());
-          }
-          else
-          {
-            urlAndQuery.Add(value.Key.ToString(), value.Value);
-          }          
-        }
-      }
-
-      Dictionary<string, string> queries = new Dictionary<string, string>();
-      if (urlAndQuery.ContainsKey("query"))
-      {
-        foreach (var query in (YamlMappingNode)urlAndQuery["query"])
-        {
-          queries.Add(query.Key.ToString(), query.Value.ToString());
-        }
-      }
-
       string[] splitUrl = url.Split('?');
-      Regex regex = new Regex(@"(yoshiwara|kanagawa)");
-      Match m = regex.Match(url);
+      string[] path = splitUrl[0].Split('/');
+
+      string[] settingPath = splitUrl[0].Split('/');
+      //string[] settingPath = deviceDic["url"].ToString().Split('/');
+            
+      Regex reg = new Regex(@"(yoshiwara|kanagawa)");
+      Match m = reg.Match(url);
       
       if (m.Success)
       {
-        Console.WriteLine("{0,-10} : {1}", m.Value, m.Result("{area:$0}"));
+        //Console.WriteLine("{0,-10} : {1}", m.Value, m.Result("{area:$0}"));
       }
-      string[] splitQuery = splitUrl[1].Split('&');
+
+      queryMatch(splitUrl[1].Split('&'));
+
+      //Console.WriteLine(settingPath.Any(x => path.Contains(x)));
+
+      return settingPath.All(x => path.Contains(x));
+    }
+
+    private bool queryMatch(string[] splitQuery)
+    {
+      foreach (var query in splitQuery)
+      {
+
+      }
 
       return false;
     }
@@ -130,6 +141,12 @@ namespace Sdx.Web
     public string GetUrl(Device device)
     {
       return "";
+    }
+
+    private string ObtainStatus(Device value)
+    {
+      string[] values = { "pc", "sp", "mb" };
+      return values[(int)value];
     }
   }
 }
