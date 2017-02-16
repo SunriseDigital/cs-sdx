99090b0dd67c4e6d5768ec8a3e5eedf67ac6a349
Masamoto Miyata <miyata@sincere-co.com>
2017-02-16 11:03:39 +0900

    テーブルクラスのインスタンス組み立てまで実装。

--- GenOrm/Program.cs
+++ GenOrm/Program.cs
@@ -19,9 +19,92 @@ namespace GenOrm
 
         foreach (var table in GetTargetTableNames(options, db))
         {
-          Sdx.Context.Current.Debug.Log(table);
+          var tableClass = CreateTableClass(table, options, db);
+          Sdx.Context.Current.Debug.Log(tableClass.Render());
         }
       });
+    }
+
+    private static Sdx.Gen.Code.File CreateTableClass(string tableName, Options options, Sdx.Db.Adapter.Base db)
+    {
+      var className = Sdx.Util.String.ToCamelCase(tableName);
+      var file = new Sdx.Gen.Code.File();
+
+      file.AddChild("using System;");
+      file.AddChild("using System.Collections.Generic;");
+      file.AddChild("using System.Linq");
+      file.AddBlankLine();
+
+      var bNamespace = new Sdx.Gen.Code.Block("namespace {0}.Table", options.Namespace);
+      file.AddChild(bNamespace);
+
+      var bClass = new Sdx.Gen.Code.Block("public class {0} : Sdx.Db.Table", className);
+      bNamespace.AddChild(bClass);
+      bClass.AddChild("public static Sdx.Db.TableMeta Meta { get; private set; }");
+
+      var bClassCtor = new Sdx.Gen.Code.Block("static {0}()", className);
+      bClass.AddChild(bClassCtor);
+
+      var bCreateTableMeta = new Sdx.Gen.Code.Block("Meta =  new Sdx.Db.TableMeta");
+      bClassCtor.AddChild(bCreateTableMeta);
+      bCreateTableMeta.ChangeDilimiter("(", ");");
+      bCreateTableMeta.StartLineBreak = false;
+      bCreateTableMeta.AddChild(@"""{0}"",", tableName);
+
+      var bColumnList = new Sdx.Gen.Code.Block("new List<Column>()");
+      bCreateTableMeta.AddChild(bColumnList);
+      bColumnList.EndDelimiter = "},";
+      foreach (var column in GetColumns(tableName, db))
+      {
+        bColumnList.AddChild(BuildCreateColumnString(column));
+      }
+
+      var bRelation = new Sdx.Gen.Code.Block("new Dictionary<string, Relation>()");
+      bCreateTableMeta.AddChild(bRelation);
+      bRelation.EndDelimiter = "},";
+
+      bCreateTableMeta.AddChild("typeof(Test.Orm.{0}),", className);
+      bCreateTableMeta.AddChild("typeof(Test.Orm.Table.{0}),", className);
+
+      return file;
+    }
+
+    private static string BuildCreateColumnString(Sdx.Db.Table.Column column)
+    {
+      var builder = new StringBuilder();
+      builder.Append(@"new Column(");
+      builder.Append('"');
+      builder.Append(column.Name);
+      builder.Append('"');
+
+      if(column.Type != null)
+      {
+        builder.Append(", type: ColumnType.");
+        builder.Append(column.Type.ToString());
+      }
+
+      if(column.IsAutoIncrement)
+      {
+        builder.Append(", isAutoIncrement: true");
+      }
+
+      if (column.IsPkey)
+      {
+        builder.Append(", isPkey: true");
+      }
+
+      if (column.IsNotNull)
+      {
+        builder.Append(", isNotNull: true");
+      }
+
+      if(column.MaxLength != null)
+      {
+        builder.Append(", maxLength: ");
+        builder.Append(column.MaxLength);
+      }
+
+      return builder.Append("),").ToString();
     }
 
     private static IEnumerable<Sdx.Db.Table.Column> GetColumns(string tableName, Sdx.Db.Adapter.Base db)
