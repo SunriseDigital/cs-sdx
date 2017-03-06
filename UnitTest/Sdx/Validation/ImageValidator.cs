b19e6173b8e29bb05cb8f171eaf3029427623e54
matsukawa <mtkwsny.sunrize@gmail.com>
2017-03-06 10:19:32 +0900

    ユニットテストのクラスを作成。

---
+++ UnitTest/Sdx/Validation/ImageValidator.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using UnitTest.DummyClasses;
+
+#if ON_VISUAL_STUDIO
+using FactAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using ClassInitializeAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using ClassCleanupAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+#endif
+
+using System;
+using System.Threading;
+using System.Globalization;
+
+namespace UnitTest
+{
+  [TestClass]
+  public class Validation_ImageValidator : BaseTest
+  {
+    [ClassInitialize]
+    public static void InitilizeClass(TestContext context)
+    {
+      Console.WriteLine("FixtureSetUp");
+      //最初のテストメソッドを実行する前に一回だけ実行したい処理はここ
+    }
+
+    [ClassCleanup]
+    public static void CleanupClass()
+    {
+      Console.WriteLine("FixtureTearDown");
+      //全てのテストメソッドが実行された後一回だけ実行する処理はここ
+    }
+
+    override protected void SetUp()
+    {
+      Console.WriteLine("SetUp");
+      //各テストメソッドの前に実行する処理はここ
+    }
+
+    override protected void TearDown()
+    {
+      Console.WriteLine("TearDown");
+      //各テストメソッドの後に実行する処理はここ
+    }
+
+    /// <summary>
+    /// このメソッドは消してはダメ
+    /// </summary>
+    override public void FixtureSetUp()
+    {
+      _TestTemplate.InitilizeClass(null);
+      //ここのクラス名は適宜書き換えてください。
+      //MSTestのFixtureSetUpがstaticじゃないとだめだったのでこのような構造になってます。
+    }
+
+    /// <summary>
+    /// このメソッドは消してはダメ
+    /// </summary>
+    override public void FixtureTearDown()
+    {
+      _TestTemplate.CleanupClass();
+      //@see FixtureSetUp
+    }
+
+
+    [Fact]
+    public void TestMethod1()
+    {
+      Console.WriteLine("TestMethod1");
+      Sdx.Context.Current.Culture = new CultureInfo("ja-JP");
+      var validator = new Sdx.Validation.NotEmpty();
+    }
+
+  }
+}
