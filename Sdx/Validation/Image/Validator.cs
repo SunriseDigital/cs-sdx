3820f00335e9c1d62e776c17b94371ec9900c583
matsukawa <mtkwsny.sunrize@gmail.com>
2017-03-07 18:53:48 +0900

    Sdx.Validation.Image.ValidatorとSdx.Validation.Image.ValidatorSetを実装。
    問題があるかもだが、ひとまず流れは実装できたと思う。

--- Sdx/Validation/Image/Validator.cs
+++ Sdx/Validation/Image/Validator.cs
@@ -5,10 +5,27 @@ using System.Text;
 using Sdx.Html;
 using System.Reflection;
 using System.IO;
+using System.Drawing;
 
 namespace Sdx.Validation.Image
 {
   public abstract class Validator : Base.Validator
   {
+    public Sdx.Image Value { get; private set; }
+
+    protected abstract bool IsValidImage(Sdx.Image value);
+
+    public bool IsValid(Sdx.Image value)
+    {
+      if (this.Errors == null)
+      {
+        this.Errors = new Errors();
+      }
+
+      Value = value;
+
+      return IsValidImage(value);
+    }
+
   }
 }
--- Sdx/Validation/Image/ValidatorSet.cs
+++ Sdx/Validation/Image/ValidatorSet.cs
@@ -8,5 +8,68 @@ namespace Sdx.Validation.Image
 {
   class ValidatorSet
   {
+    private List<Dictionary<string, object>> validators = new List<Dictionary<string, object>>();
+    public Validation.Errors Errors { get; private set; }
+    public bool IsAllowEmpty { get; set; }
+
+    public ValidatorSet()
+    {
+      this.Errors = new Validation.Errors();
+      this.IsAllowEmpty = false;
+    }
+
+    public IEnumerable<Validation.Validator> Validators
+    {
+      get
+      {
+        foreach(var dic in validators)
+        {
+          yield return (Validation.Validator)dic["validator"];
+        }
+      }
+    }
+
+    public ValidatorSet AddValidator(Validation.Validator validator, bool breakChain = false)
+    {
+      validators.Add(new Dictionary<string, object> {
+        {"validator", validator},
+        {"breakChain", breakChain}
+      });
+
+      return this;
+    }
+
+
+    public bool IsValid(Sdx.Image image)
+    {
+      var result = true;
+      this.Errors.Clear();
+
+      if(this.IsAllowEmpty)
+      {
+        return true;
+      }
+
+      foreach (var val in validators)
+      {
+        var validator = (Sdx.Validation.Image.Validator)val["validator"];
+        var breakChain = (bool)val["breakChain"];
+
+        validator.Errors = this.Errors;
+        bool isValid = validator.IsValid(image);
+
+        if (!isValid)
+        {
+          validator.Errors = null;
+          result = false;
+          if (breakChain)
+          {
+            break;
+          }
+        }
+      }
+
+      return result;
+    }
   }
 }
