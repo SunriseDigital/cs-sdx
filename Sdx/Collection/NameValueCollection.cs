7e9c7c9a6e488eb5db6333825f30224f12cc22af
Masamoto Miyata <miyata@sincere-co.com>
2017-03-24 16:08:52 +0900

    intと日付にキャストして帰るメソッドを作成。

---
+++ Sdx/Collection/NameValueCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdx.Collection
+{
+  public class NameValueCollection : System.Collections.Specialized.NameValueCollection
+  {
+    public int GetInt32Value(string key)
+    {
+      return Int32.Parse(this[key]);
+    }
+
+    public bool IsInt32(string key)
+    {
+      return GetValues(key).All(val => {
+        int i;
+        return Int32.TryParse(val, out i);
+      });
+    }
+
+    public IEnumerable<int> GetInt32Values(string key)
+    {
+      return GetValues(key).Select(val => Int32.Parse(val));
+    }
+
+    public bool IsDateTime(string key)
+    {
+      return GetValues(key).All(val =>
+      {
+        DateTime i;
+        return DateTime.TryParse(val, out i);
+      });
+    }
+
+    public DateTime GetDateTimeValue(string key)
+    {
+      return DateTime.Parse(this[key]);
+    }
+
+    public IEnumerable<DateTime> GetDateTimeValues(string key)
+    {
+      return GetValues(key).Select(val => DateTime.Parse(val));
+    }
+  }
+}
---
+++ UnitTest/Sdx/Collection/NameValueCollection.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using UnitTest.DummyClasses;
+
+#if ON_VISUAL_STUDIO
+using FactAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using ClassInitializeAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using ClassCleanupAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+#endif
+
+using System;
+
+namespace UnitTest
+{
+  [TestClass]
+  public class Collection_NameValueCollection : BaseTest
+  {
+    /// <summary>
+    /// このメソッドは消してはダメ
+    /// </summary>
+    override public void FixtureSetUp()
+    {
+      _TestTemplate.InitilizeClass(null);
+      //ここのクラス名は適宜書き換えてください。
+      //MSTestのFixtureSetUpがstaticじゃないとだめだったのでこのような構造になってます。
+    }
+
+    /// <summary>
+    /// このメソッドは消してはダメ
+    /// </summary>
+    override public void FixtureTearDown()
+    {
+      _TestTemplate.CleanupClass();
+      //@see FixtureSetUp
+    }
+
+
+    [Fact]
+    public void TestType()
+    {
+      var param = new Sdx.Collection.NameValueCollection();
+      param.Add("int", "1");
+      param.Add("datetime", "2017-03-01");
+
+      Assert.True(param.IsInt32("int"));
+      Assert.Equal(1, param.GetInt32Value("int"));
+      Assert.Equal(new int[]{1}, param.GetInt32Values("int"));
+
+      Assert.True(param.IsDateTime("datetime"));
+      Assert.Equal("2017-03-01", param.GetDateTimeValue("datetime").ToString("yyyy-MM-dd"));
+      Assert.Equal(new string[] { "2017-03-01" }, param.GetDateTimeValues("datetime").Select(val => val.ToString("yyyy-MM-dd")));
+    }
+  }
+}
