637d9e87d6adb30ca60b9b4aa1a337825a1ce992
tkabeee <tkabee@gmail.com>
2017-05-18 12:41:20 +0900

    URLエンコードメソッドの間違い

--- Sdx/Util/String.cs
+++ Sdx/Util/String.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Web;
 
 namespace Sdx.Util
 {
@@ -117,7 +118,7 @@ namespace Sdx.Util
         }
       }
 
-      return UrlEncode(str);
+      return HttpUtility.UrlEncode(str);
     }
   }
 }
