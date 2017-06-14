16e9a72ba9b9f8286dbb5bc283a2f073183b387b
matsukawa <mtkwsny.sunrize@gmail.com>
2017-06-14 13:01:16 +0900

    bitフラグ判定をするメソッドを作成

---
+++ Sdx/Util/Bit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sdx.Util
+{
+  public static class Bit
+  {
+    /// <summary>
+    /// flagsにフラグ立っているか否かを調べます。
+    /// </summary>
+    /// <param name="flags"></param>
+    /// <param name="flag"></param>
+    /// <returns></returns>
+    public static bool HasBit(int flags, int flag)
+    {
+      return (flags & flag ) == flag;;
+    }
+  }
+}
