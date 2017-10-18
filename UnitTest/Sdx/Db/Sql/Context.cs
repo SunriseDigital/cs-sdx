b5445f592aed709bfe9341839adab7631768b001
tomita-ychr <sunrisedigitaltomita@gmail.com>
2017-10-18 14:43:36 +0900

    Sdx.Db.Sql.Context のユニットテスト用クラスを追加
    ありそうでまだなかった。

---
+++ UnitTest/Sdx/Db/Sql/Context.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using UnitTest.DummyClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+#if ON_VISUAL_STUDIO
+using FactAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TestClassAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using ClassInitializeAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+using ClassCleanupAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+#endif
+
+namespace UnitTest
+{
+  [TestClass]
+  public class Db_Sql_Context : BaseDbTest
+  {
+    [ClassInitialize]
+    public new static void InitilizeClass(TestContext context)
+    {
+      BaseDbTest.InitilizeClass(context);
+    }
+  }
+}
